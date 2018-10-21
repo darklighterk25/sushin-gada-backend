f2d8f1b6471a3653aa32306684b13631f1034d6e
Eduardo Valdez <iryx_clow@hotmail.com>
2018-10-20 22:38:20 -0500

    Método de login
    Método de login

--- routes/account.js
+++ routes/account.js
@@ -21,4 +21,41 @@ router.get('/', (req, res) => {
     );
 });
 
+router.post('/login', (req, res) => {
+    database.query(`SELECT id_user, account_type FROM user where email = '${req.headers['email']}' and password = '${req.headers['password']}'`, (err, result) => {
+        if (err) throw err;
+        if (result.length === 1) {
+            var employee;
+            if (result[0].account_type === 0) {
+                employee = 'true';
+            } else {
+                employee = 'false';
+            }
+            res.status(200).json(
+                {
+                    "loggedIn": "true",
+                    "employee": `${employee}`,
+                    "id_user": `${result[0].id_user}`
+                }
+            );
+        } else if (result.length === 0) {
+            res.status(200).json(
+                {
+                    "loggedIn": "false",
+                    "employee": "-1",
+                    "id_user": "-1"
+                }
+            );
+        } else {
+            res.status(200).json(
+                {
+                    "loggedIn": "null",
+                    "employee": "-1",
+                    "id_user": "-1"
+                }
+            );
+        }
+    });
+});
+
 module.exports = router;
