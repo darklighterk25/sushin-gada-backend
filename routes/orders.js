70fae3b2b385e3fcd6601743120d7159ff12ca84
Eduardo Valdez <iryx_clow@hotmail.com>
2018-12-09 00:33:28 -0600

    Petición de pago
    Se definió la petición que se utiliza al pagar una orden
    Eliminación de console.log
    
    Se eliminó un console.log

--- routes/orders.js
+++ routes/orders.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const database = require('../database');
 const router = express.Router();
+const simplify = require("simplify-commerce")
 
 router.get('/', (req, res) => {
     res.status(200).json(
-        [
-            {
+        [{
                 "id": "2085",
                 "date": new Date('August 25, 2018 14:24:00'),
-                "items": [
-                    {
+                "items": [{
                         "name": "Mango Avocado Uramaki",
                         "type": "dish",
                         "description": "Uramaki de mango y aguacate",
@@ -31,16 +30,14 @@ router.get('/', (req, res) => {
             {
                 "id": "6574",
                 "date": new Date('August 12, 2018 15:05:55'),
-                "items": [
-                    {
-                        "name": "Unagui",
-                        "type": "dish",
-                        "description": "Rollo de alga, salmón y anguila.",
-                        "picture": "https://imgur.com/oogRI5R.jpg",
-                        "quantity": 1,
-                        "price": 120
-                    }
-                ],
+                "items": [{
+                    "name": "Unagui",
+                    "type": "dish",
+                    "description": "Rollo de alga, salmón y anguila.",
+                    "picture": "https://imgur.com/oogRI5R.jpg",
+                    "quantity": 1,
+                    "price": 120
+                }],
                 "total": 120
             },
         ]
@@ -48,11 +45,29 @@ router.get('/', (req, res) => {
 });
 
 router.put('/purchase', (req, res) => {
-    res.status(200).json(
-        {
-            "status": 200
+    var client = simplify.getClient({
+        publicKey: 'sbpb_MzI5MzYzMGQtMTg2YS00YTg1LWIxN2ItOTc5YzI2YmE4NzA4',
+        privateKey: '7kzSZa+BU/e+k/CWw2TV09muK0t3Cgqv+EyO8rkQYIZ5YFFQL0ODSXAOkNtXTToq'
+    });
+    client.payment.create({
+        amount: `${req.body['amount']}`,
+        description: `${req.body['description']}`,
+        card: {
+            expMonth: `${req.body['card'].expMonth}`,
+            expYear: `${req.body['card'].expYear}`,
+            cvc: `${req.body['card'].ccv}`,
+            number: `${req.body['card'].number}`
         }
-    );
+    }, (errData, data) => {
+        if (errData) {
+            res.status(401).json({
+                "status": 401,
+                "message": `${errData.data.error.message}`
+            });
+        } else {
+            res.status(200).json(data);
+        }
+    });
 });
 
 module.exports = router;
