34f8d8ed7614f18a200219c35e49f2e7b73caae2
Eduardo Valdez <iryx_clow@hotmail.com>
2018-12-10 23:28:43 -0600

    Mejor en Triggers y Procedimientos
    Se mejoraron los triggers así como sus nombres, también se gregaron unas leves mejoras a los stored procedures para que trabajen de manera adecuada junto con los triggers

--- procedures.sql
+++ procedures.sql
@@ -1,19 +1,18 @@
 -- Procedimiento para registrar un nuevo usuario
 create procedure sign_up(
-  in
-    street varchar(50),
-    number varchar(7),
-    interior_number varchar(5),
-    neighborhood varchar(50),
-    zip_code char(6),
-    phone char(10),
-    name varchar(45),
-    last_name1 varchar(45),
-    last_name2 varchar(45),
-    birthdate date,
-    gender tinyint(4),
-    email varchar(45),
-    password varchar(64)
+  street varchar(50),
+  number varchar(7),
+  interior_number varchar(5),
+  neighborhood varchar(50),
+  zip_code char(6),
+  phone char(10),
+  name varchar(45),
+  last_name1 varchar(45),
+  last_name2 varchar(45),
+  birthdate date,
+  gender tinyint(4),
+  email varchar(45),
+  password varchar(64)
 )
 begin
   declare addr int;
@@ -22,56 +21,37 @@ begin
   insert into user values(id_user, addr, name, last_name1, last_name2, birthdate, gender, email, password, 1, 1);
 end
 
--- procedimiento para insertar nuevos items al carrito
+-- Procedimiento para insertar nuevos items al carrito
 create procedure add_to_cart(
-  
-  in
-    
-    user int,
-
-    id_item tinyint(4),
-
-    quantity tinyint(4),
-
-    price decimal(5,2)
-
+  user int,
+  new_item tinyint(4),
+  new_quantity tinyint(4),
+  unit_price decimal(5,2)
 )
-
 begin
-
   declare subtotal decimal(5,2);
-
   declare aux_purchase int;
-
-  set subtotal = quantity * price;
-
+  declare aux_items int;
+  set subtotal = new_quantity * unit_price;
   select id_purchase from purchase where closed = 0 and id_user = user into aux_purchase;
-
   if aux_purchase is null then
-
     insert into purchase values(id_purchase, user, null, 0, 0, now(), 0, 0, 0, null);
-
     select id_purchase from purchase where closed = 0 and id_user = user into aux_purchase;
-
   end if;
-
-  insert into items values(id_items, aux_purchase, id_item, quantity, subtotal, price);
-
+  select id_items from items where id_purchase = aux_purchase and id_item = new_item into aux_items;
+  if aux_items is null then
+    insert into items values(id_items, aux_purchase, new_item, new_quantity, subtotal, unit_price);
+  else
+    update items set quantity = quantity + new_quantity, subtotal = quantity * price where id_items = aux_items;
+  end if;
 end
 
 -- Procedimiento para actualizar el total y taxes en las compras
-create procedure update_purchase(in purchase_key int)
-
+create procedure update_purchase(purchase_key int)
 begin
-
   declare aux_total varchar(45);
-
   declare aux_taxes varchar(45);
-
   select sum(subtotal) from items where id_purchase = purchase_key into aux_total;
-
   set aux_taxes = aux_total * 0.16;
-
   update purchase set total = aux_total, taxes = aux_taxes where id_purchase = purchase_key;
-
 end
--- triggers.sql
+++ triggers.sql
@@ -1,64 +1,48 @@
--- trigger que actualiza el total y taxes acumulados de una orden
-create trigger purchase_trigger after insert on items
-
+-- Trigger que actualiza el total y taxes acumulados de una orden
+-- despues de insertar un nuevo item al carrito.
+create trigger purchase_trigger_ins after insert on items
 for each row
-
 begin
-
-  call update_purchase(new.id_purchase);
-
-end
-
--- trigger que actualiza el total y taxes de una orden al cambiar el precio del menu
-create trigger update_order_total after update on items
-
-for each row
-
-begin
-
 	call update_purchase(new.id_purchase);
-
 end
 
--- trigger que actualiza los precios en el carrito cuando aun no se haga la compra
-create trigger update_prices before update on menu
-
+-- Trigger que actualiza el total y taxes acumulados de una orden
+-- despues de actualizar algun registro del carrito.
+create trigger purchase_trigger_upd after update on items
 for each row
-
 begin
+	call update_purchase(new.id_purchase);
+end
 
+-- Trigger que actualiza el total y taxes acumulados de una orden
+-- después de eliminar algun item del carrito.
+create trigger purchase_trigger_del after delete on items
+for each row
+begin
+	call update_purchase(old.id_purchase);
+end
+
+-- Trigger que actualiza los precios de compra en el carrito siempre
+-- que aun no se haya pagado la orden.
+create trigger update_prices before update on menu
+for each row
+begin
 	declare done int default false;
-
 	declare c_closed int;
 	declare c_purchase int;
-
 	declare c_cursor cursor for select closed, id_purchase from purchase;
 	declare continue handler for not found set done = true;
-
     	open c_cursor;
-
     	if new.price != old.price then
-
 		read_loop: loop
-
 			fetch c_cursor into c_closed, c_purchase;
-
             		if done then
-
 				leave read_loop;
-
 			end if;
-
 			if c_closed = 0 then
-
 				update items set price = new.price, subtotal = new.price*quantity where id_item = new.id_item and id_purchase = c_purchase;
-
             		end if;
-
 		end loop;
-
 	end if;
-
     close c_cursor;
-
 end
